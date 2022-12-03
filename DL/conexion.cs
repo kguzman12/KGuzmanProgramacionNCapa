89a4ed395bbe5427eb33ce6c8109e708ac826a4c
kguzman12 <akarenguzmn@gmail.com>
2022-12-02 19:18:07 -0600

    conexion

---
+++ DL/conexion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    public class conexion
+    {
+        public static string GetConnectionString()//Buscar la cadena de conexión
+        {
+            return ConfigurationManager.ConnectionStrings["KGuzmanProgramacionNCapas"].ConnectionString.ToString();
+        }
+    }
+}
